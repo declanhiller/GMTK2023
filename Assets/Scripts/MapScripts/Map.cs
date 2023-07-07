daea51d7b8ec609ae862ac7754c5be2910a4c1da
declanhiller <declan.hiller@gmail.com>
2023-07-07 17:00:11 -0600

    test of palette

--- Assets/Scripts/MapScripts/Map.cs
+++ Assets/Scripts/MapScripts/Map.cs
@@ -6,21 +6,19 @@ namespace MapScripts {
 
         [SerializeField] private int width;
         [SerializeField] private int height;
+
+        [SerializeField] private Vector2Int startTile;
         
-        private Node[] _node;
+        private Grid grid;
 
         private void Start() {
-            GenerateNodes();
+            grid = GetComponent<Grid>();
+            grid.GetCellCenterWorld((Vector3Int) startTile);
+            
+            
         }
-
-        private void GenerateNodes() {
-            for (int i = 0; i < width; i++) {
-                for (int j = 0; j < height; j++) {
-                    GameObject node = new GameObject("Node [" + i + ", " + j + "]");
-                    node.AddComponent<Node>();
-                }
-            }
-        }
+        
+        
 
     }
 }
--- Assets/Scripts/MapScripts/Node.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace MapScripts {
-    public class Node : MonoBehaviour {
-
-        [SerializeField] private BoxCollider2D collider;
-
-    }
-}
