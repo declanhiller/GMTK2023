47a2766992b9857dd3d1fc3ca11ad4e116cdccda
declanhiller <declan.hiller@gmail.com>
2023-07-09 09:36:03 -0600

    Start Screen

--- Assets/StartScreenManager.cs
+++ Assets/StartScreenManager.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartScreenManager : MonoBehaviour
 {
     public void StartGame() {
-        
+        SceneManager.LoadScene("CutScene");
     }
 
     public void ExitGame() {
